d09586e9846dbb91391876ba9f9f19d47b8be757
Nicholas Barger <nicholas@nicholasbarger.com>
2015-05-06 23:29:14 -0400

    Added graphics for homepage - still working on jquery validation

--- public/scripts/controls.js
+++ public/scripts/controls.js
@@ -1,10 +1,19 @@
 // initialize javascript controls
 $(function() {
   $('.datetimepicker').datetimepicker();
+
   $('.carousel').slick({
     autoplay: true,
     autoplaySpeed: 2000,
     lazyLoad: 'ondemand',
     speed: 500
   });
+
+  $.validator.setDefaults({
+    submitHandler: function() {
+      console.log('form submitted');
+      form1.submit();
+    }
+  });
+  $('form').validate();
 });

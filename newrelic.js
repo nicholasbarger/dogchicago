8b88584157674caec77d444df4716eb637813b09
Nicholas Barger <nicholas@nicholasbarger.com>
2015-11-19 11:32:38 -0800

    Added new relic to monitor some of the performance issues occurring.

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Dog Chicago'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'efa012b17b5368caa60df3b51ab8652271a0c132',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
